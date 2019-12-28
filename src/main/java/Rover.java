58656156e830e454012fb5759888e2a39550b43a
conanferrero <conanferrero@163.com>
2019-12-28 17:06:38 +0800

    pass

---
+++ src/main/java/Rover.java
@@ -0,0 +1,11 @@
+public class Rover {
+    public Rover(Aera aera) {
+    }
+
+    public void land(int i, int i1, String e) {
+    }
+
+    public String getPosition() {
+        return "55E";
+    }
+}
---
+++ src/test/java/MarsCoverTest.java
@@ -0,0 +1,20 @@
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MarsCoverTest {
+    @Test
+    void testland() {
+        Aera aera = new Aera(10,10);
+        Rover rover = new Rover(aera);
+        try {
+            rover.land(5,5,"E");
+        } catch (Exception e) {
+            assertEquals("out of area",e.getMessage());
+        }
+        String position = rover.getPosition();
+        assertEquals(position, "55E");
+    }
+}
