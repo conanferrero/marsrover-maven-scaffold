5d947c78c31cab69bcb7866ac0320539a76afe42
conanferrero <conanferrero@163.com>
2019-12-28 17:29:21 +0800

    refactor

---
+++ src/main/java/RoverController.java
@@ -0,0 +1,25 @@
+public class RoverController {
+    private final Rover rover;
+
+    public RoverController(Rover rover) {
+        this.rover = rover;
+    }
+
+    public String excute(String mission) throws Exception {
+        String[] missions = mission.split(";");
+        int size = missions.length;
+        if (size < 5) throw new Exception("invalid input");
+
+        Aera aera = new Aera(Integer.valueOf(missions[0]), Integer.valueOf(missions[1]));
+        Rover rover = new Rover(aera);
+        rover.land(Integer.valueOf(missions[2]), Integer.valueOf(missions[3]), missions[4]);
+
+        if (size > 5) {
+            for (int i = 5; i < size; i++) {
+                if ("M".equals(missions[i])) rover.move();
+                else rover.turn(missions[i]);
+            }
+        }
+        return rover.getPosition();
+    }
+}
